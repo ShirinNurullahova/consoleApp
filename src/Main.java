646a9b79f910dafc9d935d19f55a301acabe820a
ShirinNurullahova <sirin.nurullahova@mail.ru>
2024-06-08 22:24:31 +0400

    menu

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,20 @@ public class Main {
     public static University university;
 
     public static void main(String[] args) {
-        university = createUniversity();
+        university = new University();
         while (true) {
             menu();
-            System.out.println("Secim et");
-            int choose = scanner.nextInt();
-            scanner.nextLine();
-            switch (choose) {
-                case 1 -> university.addStudent();
-                 case 2-> university.addEmployee();
-            }
         }
-    }
-
-    public static University createUniversity() {
-        System.out.println(" Universitet Adi daxil et");
-        String uniname = scanner.nextLine();
-        System.out.println("Ishci limiti");
-        int workerLimit = scanner.nextInt();
-        System.out.println("Maash limiti daxil et");
-        double salaryLimit = scanner.nextDouble();
-        scanner.nextLine();
-        University university = new University(uniname, workerLimit, salaryLimit, new Student[2], new Employee[2]);
-        return university;
     }
 
     public static void menu() {
         System.out.println("1. Student yarat");
         System.out.println("2. Ishci yarat");
+        System.out.println("Secim et");
+        int choose = scanner.nextInt();
+        switch (choose) {
+            case 1 -> university.addStudent();
+            case 2 -> university.addEmployee();
+        }
     }
 }
