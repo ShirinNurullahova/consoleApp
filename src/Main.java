c7709c572376e42bdcc102e3e038c7deaa9f3725
ShirinNurullahova <sirin.nurullahova@mail.ru>
2024-06-09 00:22:03 +0400

    switch case added

--- src/Main.java
+++ src/Main.java
@@ -14,11 +14,25 @@ public class Main {
     public static void menu() {
         System.out.println("1. Student yarat");
         System.out.println("2. Ishci yarat");
+        System.out.println("3. Telebelerin siyahisini goster");
+        System.out.println("4. Telebeni update et (groupNo)");
+        System.out.println("5. Butun telebelerin ortalama ballarini goster");
+        System.out.println("6. Grup telebelerinin ortalama ballarini goster");
+        System.out.println("7. Iscini sil (Employee No.)");
+        System.out.println("8. Iscilerin maas ortalamasini goster");
+        System.out.println("9. Ishchilerin siyahisini goster");
         System.out.println("Secim et");
         int choose = scanner.nextInt();
         switch (choose) {
             case 1 -> university.addStudent();
             case 2 -> university.addEmployee();
+            case 3 -> university.getAllStudents();
+            case 4 -> university.updateStudent();
+            case 5 -> university.calcStudentsAverage();
+            case 6 -> university.calcStudentsAverageByGroup();
+            case 7 -> university.deleteEmployee();
+            case 8 -> university.calcSalaryAverage();
+            case 9 -> university.getAllEmployees();
         }
     }
 }
