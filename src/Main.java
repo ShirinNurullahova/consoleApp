a8a83aad364e7f431f44be752caa1c3095a2e439
ShirinNurullahova <sirin.nurullahova@mail.ru>
2024-06-09 15:47:26 +0400

    added case

--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,15 @@ public class Main {
         System.out.println("9. Ishchilerin siyahisini goster");
         System.out.println("10.Universitetdeki ishcilerin siyahisini goster(Department Name.)");
         System.out.println("11.Telebelerin siyahisini goster(Grup No.)");
+        System.out.println("12.Ishcini update et (position ve salary)");
+        System.out.println("13.Ishci adina gore axtarish et");
+        System.out.println("14.Telebe adina gore axtarish et");
+        System.out.println("15.Telebenin balina gore axtarish et");
+        System.out.println("16.Telebenin grup no  gore axtarish et");
+        System.out.println("17.Ishci  maashina gore axtarish et");
+        System.out.println("18.Ishci depName  gore axtarish et");
+        System.out.println("19.Ishci tipine  gore axtarish et");
+        System.out.println("20.Cixish");
         System.out.println("Secim et");
         int choose = scanner.nextInt();
         switch (choose) {
@@ -37,6 +46,16 @@ public class Main {
             case 9 -> university.getAllEmployees();
             case 10-> university.getEmployeesByDepartmentName();
             case 11-> university.getStudentsByGroupNo();
+            case 12->university.updateEmployee();
+            case 13->university.searchEmployeeByName();
+            case 14->university.searchStudentByName();
+            case 15->university.searchStudentByPoint();
+            case 16->university.searchStudentByGroupNumber();
+            case 17->university.searchEmployeeBySalary();
+            case 18->university.searchEmployeeByDepartmentName();
+            case 19->university.searchEmployeeByType();
+            case 20 -> System.exit(0);
+            default -> System.out.println("Yanlish secim!");
         }
     }
 }
